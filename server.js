7cba22739075ea649f1b42553285bb91319441ce
Ridho Hafidz <mridhohafidz12@gmail.com>
2022-05-24 11:42:36 +0700

    update app
    Signed-off-by: Ridho Hafidz <mridhohafidz12@gmail.com>

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const {greeting} = require('./user')
 
-app.get('/', (req,res) => res.send('This App is running properly!'))
+app.get('/', (req,res) => res.send('This App is running!'))
 app.get('/ping', (req,res) => res.send('Pong!'))
 app.get('/youtube', (req,res) => res.send('Hello, youtube!'))
 app.get('/hello/:name', (req,res) => {
